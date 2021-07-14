ec8fd719ce9daac2d11b67b05a986474307ca8d1
Mikhail <rymv@yandex.ru>
2021-07-14 12:02:59 +0300

    lesson 1

--- 01_IntroductionToWebFrameworks/todo/tasks/views.py
+++ 01_IntroductionToWebFrameworks/todo/tasks/views.py
@@ -7,8 +7,9 @@ class ToDoView(View):
 
     def get(self, request, *args, **kwargs):
         return HttpResponse('<ul>'
-                            '<li>Установить python</li>'
-                            '<li>Установить django</li>'
-                            '<li>Запустить сервер</li>'
-                            '<li>Порадоваться результату</li>'
+                            '<li>Установить python. Был установлен.</li>'
+                            '<li>Установить django. Установил.</li>'
+                            '<li>Запустить сервер. Запустил.</li>'
+                            '<li>Порадоваться результату. Ура!!!</li>'
+                            '<li>5-ый элемент</li>'
                             '</ul>')
