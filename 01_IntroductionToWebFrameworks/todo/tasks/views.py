d187d5b016e188a61598bd32e910ea8439aa837b
Mikhail <rymv@yandex.ru>
2021-07-15 15:56:04 +0300

    lesson 1 TODO

--- 01_IntroductionToWebFrameworks/todo/tasks/views.py
+++ 01_IntroductionToWebFrameworks/todo/tasks/views.py
@@ -1,15 +1,20 @@
 from django.http import HttpResponse
-
 from django.views import View
+import random
+
+possible_tasks = ['Task 1', 'Task 2', 'Task 3', 'Task 4', 'Task 5', 'Task 6', 'Task 7', 'Task 8', 'Task 9', 'Task 10']
+
+
+def random_tasks():
+    random_response = '<ul>'
+    for task in random.sample(possible_tasks, 5):
+        random_response += f'<li>{task}</li>'
+    random_response += '</ul>'
+    return random_response
 
 
 class ToDoView(View):
 
     def get(self, request, *args, **kwargs):
-        return HttpResponse('<ul>'
-                            '<li>Установить python. Был установлен.</li>'
-                            '<li>Установить django. Установил.</li>'
-                            '<li>Запустить сервер. Запустил.</li>'
-                            '<li>Порадоваться результату. Ура!!!</li>'
-                            '<li>5-ый элемент</li>'
-                            '</ul>')
+        response = random_tasks()
+        return HttpResponse(response)
