c5042e588cd544feda54a29188b3088464733aed
Mikhail <rymv@yandex.ru>
2021-07-25 11:38:22 +0300

    lesson 4 TODO

--- 04_DatabasesAndModels/board/advertisements_app/views.py
+++ 04_DatabasesAndModels/board/advertisements_app/views.py
@@ -1,5 +1,6 @@
 from django.views import generic
 from .models import Advertisement
+from django.http import Http404
 
 
 class AdvertisementListView(generic.ListView):
@@ -11,3 +12,43 @@ class AdvertisementListView(generic.ListView):
 
 class AdvertisementDetailView(generic.DetailView):
     model = Advertisement
+
+    def get_object(self, queryset=None):
+        """
+        Return the object the view is displaying.
+
+        Require `self.queryset` and a `pk` or `slug` argument in the URLconf.
+        Subclasses can override this to return any object.
+        """
+        # Use a custom queryset if provided; this is required for subclasses
+        # like DateDetailView
+        if queryset is None:
+            queryset = self.get_queryset()
+
+        # Next, try looking up by primary key.
+        pk = self.kwargs.get(self.pk_url_kwarg)
+        slug = self.kwargs.get(self.slug_url_kwarg)
+        if pk is not None:
+            queryset = queryset.filter(pk=pk)
+
+        # Next, try looking up by slug.
+        if slug is not None and (pk is None or self.query_pk_and_slug):
+            slug_field = self.get_slug_field()
+            queryset = queryset.filter(**{slug_field: slug})
+
+        # If none of those are defined, it's an error.
+        if pk is None and slug is None:
+            raise AttributeError(
+                "Generic detail view %s must be called with either an object "
+                "pk or a slug in the URLconf." % self.__class__.__name__
+            )
+
+        try:
+            # Get the single item from the filtered queryset
+            obj = queryset.get()
+        except queryset.model.DoesNotExist:
+            raise Http404(_("No %(verbose_name)s found matching the query") %
+                          {'verbose_name': queryset.model._meta.verbose_name})
+        obj.views_count += 1
+        obj.save()
+        return obj
